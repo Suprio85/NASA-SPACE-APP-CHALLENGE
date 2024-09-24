40d18b22a15c26898a5458e233a78c0ec5eb9ee6
Suprio Paul <2105085@ugrad.cse.buet.ac.bd>
2024-09-25 02:10:49 +0600

    error Handler Updated

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,3 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const errHandeler = (err, req, res, next) => {
+    const statusCode = res.statusCode? res.statusCode : 500;
+    
+    res.status(statusCode)
+    
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    });
+};
+
 
 // add this to the end of the file of backend/server.js
 // demo:
@@ -6,14 +20,3 @@
 // app.use(errHandeler);
 // remember to put this at the end of the file, after all the routes and middlewares
 // otherwise, it will not work as expected 
-
-export const errHandeler = (err, req, res, next) => {
-    const statusCode = res.statusCode? res.statusCode : 500;
-
-    res.status(statusCode)
-    
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
-    });
-};
