78fcda519825944acdeca763a7b0b0b79cbcdedc
MD MEHEDI HASAN MIM <mdmehedihasanmim2022@gmail.com>
2024-09-27 15:49:08 +0600

    design done

---
+++ frontend/src/component/title.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Title = ({text}) => {
+    return (
+        <div className=' mt-20'>
+            <div className='font-suse font-bold text-slate-950 text-3xl '>{text} </div>
+        </div>
+    )
+}
+export default Title;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,29 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily:{
+      bebasNue: ['Bebas Neue'],
+      suse: ['SUSE'],
+      spaceGrotesk: ['Space Grotesk'],
+      NanumGothic: ['Nanum Gothic'],
+      Saira :['Saira'],
+      Titiliuam:['Titillium Web'],
+      Roboto:['Roboto'],
+      Opensans:['Open Sans'],
+    }
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none', /* Internet Explorer 10+ */
+          'scrollbar-width': 'none', /* Firefox */
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none', /* Safari and Chrome */
+        },
+      });
+    },
+  ],
 }
