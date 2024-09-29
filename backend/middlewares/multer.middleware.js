22ec21087b293f08da01f8e25bebbdbe8613843d
Shahir BIn Zulfiker <2105149@ugrad.cse.buet.ac.bd>
2024-09-30 01:12:18 +0600

    file directory issue resolved

--- backend/middlewares/multer.middleware.js
+++ backend/middlewares/multer.middleware.js
@@ -1,15 +1,22 @@
 import multer from "multer";
+import path from "path";
+import { fileURLToPath } from 'url';
 
+// Get the current file's directory equivalent to __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Set up relative path for the destination directory
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/Users/shahirbinzulfikeraorko/Desktop/Backend/public/temp")
+    const destinationPath = path.join(__dirname, "../public/temp");
+    cb(null, destinationPath);
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname)
+    cb(null, file.originalname);
   }
-})
+});
 
-  
 export const upload = multer({ 
-    storage, 
-})
+  storage, 
+});
