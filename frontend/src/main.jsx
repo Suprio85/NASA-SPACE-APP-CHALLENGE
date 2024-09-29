d499603f366aa15144441fd7ccd702f5b3eceb1f
Suprio Paul <2105085@ugrad.cse.buet.ac.bd>
2024-09-29 20:11:45 +0600

    Chatbot and planet final coomit before merging

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,11 @@ import { StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Planets from './component/Planets.jsx'
-import Chatbot from './component/Chatbot.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Chatbot/>
+    <App/>
   </StrictMode>,
 )
