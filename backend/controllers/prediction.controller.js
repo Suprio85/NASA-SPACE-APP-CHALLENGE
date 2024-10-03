e6814de65ee1ed32fb39f0b5fbfb6016029b57c2
Suprio Paul <2105085@ugrad.cse.buet.ac.bd>
2024-10-03 23:42:07 +0600

    ektu age commit korsi. Aorko er jonno abar commit korte hocce. bal

--- backend/controllers/prediction.controller.js
+++ backend/controllers/prediction.controller.js
@@ -42,4 +42,43 @@ const predictExoplanetType = asyncHandler(async (req, res) => {
     });
 });
 
+
+
+
+const predictExoplanetName = asyncHandler(async (req, res) => {
+    const {  } = req.body;
+
+    const pathDir = path.join(__dirname, "..", 'scripts','MakeYourExoplanet', 'predict.py');
+    console.log(pathDir);
+    
+    const pythonProcess = spawn('python', [pathDir, distance, mass, radius, orbital]);
+
+    let predictionResult = '';
+    let errorOccurred = false;
+
+    // Capture stdout data
+    pythonProcess.stdout.on('data', (data) => {
+        predictionResult += data.toString();
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`Error: ${data}`);
+        errorOccurred = true;
+    });
+    pythonProcess.on('close', (code) => {
+        console.log(`Python script exited with code ${code}`);
+        
+        if (errorOccurred) {
+            return res.status(500).json({ message: "Error in prediction" });
+        }
+
+        // Only return the final prediction to the client
+        const outputLines = predictionResult.trim().split('\n');
+        const finalPrediction = outputLines[outputLines.length - 1];  // Get the last line, which should be the prediction
+        console.log(`Final prediction: ${finalPrediction}`);
+
+        res.json({ prediction: finalPrediction });
+    });
+});
+
 export { predictExoplanetType };
